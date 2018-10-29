58a066c9bb194032405243c248f1fb0618b9d22f
kef <>
2018-10-29 13:50:48 +0300

    Add layer layout save/load

--- domainmodel.py
+++ domainmodel.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
+import os
 
 from PyQt5.QtCore import QObject, pyqtSignal
 from attr import attrs, attrib
-from numpy import linspace, ndarray
+from numpy import linspace
 from tmm import coh_tmm, inf
 
 
@@ -31,15 +31,31 @@ class DomainModel(QObject):
         self._lambdas = list()
         self._Rn = list()
 
-    def init(self):
+    def init(self, filename='./default_layers.txt'):
         print("init domain model")
-
-        self._layers = [
-            Layer(thick=inf, refract=1.0 + 0j),
-            Layer(thick=100, refract=2.78 + 0j),
-            Layer(thick=100, refract=3.0 + 3.3j),
-            Layer(thick=inf, refract=10000 + 0j),
-        ]
+        self._layers.clear()
+        if os.path.isfile(filename):
+            with open(filename, 'rt', encoding='utf-8') as f:
+                lines = [line.strip() for line in f.readlines()]
+                if len(lines) > 2:
+                    if 'inf;' in lines[0] and 'inf;' in lines[-1]:
+                        for line in lines:
+                            thick, refract = line.split(';')
+                            if ' ' in thick:
+                                print('error parsing data file', thick)
+                                break
+                            if thick == 'inf':
+                                thick = inf
+                            else:
+                                thick = int(thick)
+                            self._layers.append(Layer(thick=thick, refract=complex(refract)))
+        else:
+            self._layers = [
+                Layer(thick=inf, refract=1.0 + 0j),
+                Layer(thick=100, refract=2.78 + 0j),
+                Layer(thick=100, refract=3.0 + 3.3j),
+                Layer(thick=inf, refract=10000 + 0j),
+            ]
 
         self._prepLists()
         self._calcReflect()
@@ -60,6 +76,12 @@ class DomainModel(QObject):
             self._Rn.append(coh_tmm('s', self._refracts, self._thicks, 0, l)['R'])
 
         self.dataReady.emit()
+
+    def saveLayers(self, filename):
+        with open(filename, 'wt', encoding='utf-8') as f:
+            for layer in self._layers:
+                line = f'{layer.thick};{str(layer.refract).strip("()")}\n'
+                f.write(line)
 
     def addLayer(self, row: int):
         self._layers.insert(row, Layer(100, 1.5))
